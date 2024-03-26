5002806a5a642a835a0474e17200cc937fc1f0da
Ondatra Eating My Head <51259756+HasAzRus@users.noreply.github.com>
2024-03-26 23:46:08 +0500

    Добавил Behaviour

---
+++ Assets/Scripts/Core/Behaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Homework
+{
+    public class Behaviour : MonoBehaviour
+    {
+        protected virtual void OnEnable()
+        {
+            
+        }
+
+        protected virtual void Awake()
+        {
+            
+        }
+
+        protected virtual void Start()
+        {
+            
+        }
+
+        protected virtual void Update()
+        {
+            
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            
+        }
+
+        protected virtual void OnDisable()
+        {
+            
+        }
+
+        protected virtual void OnDestroy()
+        {
+            
+        }
+    }
+}
